02eb9b9fb0643183e3e97d6e7e18ff0bf0c703f5
Jon Ward <jon@gamingrealms.com>
2016-06-14 08:06:11 +0100

    Add Hipchat notifications

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # check-quarantine
-Logs in and checks quarantine of a Google Apps account. Sends an email notification if there are quarantined messages.
+Logs in and checks quarantine of a Google Apps account. Prints summary of quarantined messages quarantined messages to stdout. Optionally sends an email or Hipchat notification.
 
 This script uses Watir-Webdriver in Headless mode since there is no API coverage of the quarantine functionality.
+
+# dependencies
+$ sudo apt-get install ruby-dev xvfb
+
+$ sudo gem install watir-webdriver headless hipchat
+
+
+an smtp server must be available for email notifications to work
--- check_quarantine.rb
+++ check_quarantine.rb
@@ -1,21 +1,49 @@
 require 'watir-webdriver'
 require 'headless'
 require 'net/smtp'
+require 'hipchat'
 
-LOGIN_URL = "https://accounts.google.com/login#identifier"
-QUARANTINE_URL = "https://email-quarantine.google.com/adminreview"
-LOGIN_EMAIL = ""
-LOGIN_PASSWORD = ""
+#Google Apps settings
+LOGIN_URL      = 'https://accounts.google.com/login#identifier'
+QUARANTINE_URL = 'https://email-quarantine.google.com/adminreview'
+LOGIN_EMAIL    = ''
+LOGIN_PASSWORD = ''
 
-SENDER_NAME = ""
-SENDER_EMAIL = ""
-RECEIVER_NAME = ""
-RECEIVER_EMAIL = ""
-EMAIL_SUBJECT = "Quarantined emails #{Time.now}"
+#Email notification settings
+SEND_EMAIL     = false
+SENDER_NAME    = ''
+SENDER_EMAIL   = ''
+RECEIVER_NAME  = ''
+RECEIVER_EMAIL = ''
+EMAIL_SUBJECT  = "Quarantined emails #{Time.now}"
+
+#Hipchat notification settings
+SEND_HIPCHAT_NOTIFICATION = false
+API_TOKEN                 = ''
+ROOM_ID                   = ''
+USERNAME                  = 'QuarantineBot'
+COLOR                     = 'yellow'# 'red', 'yellow', 'green', 'purple', 'random'
+
+#Print results to stdout
+PRINT_RESULTS = true
+
+def send_email(msg)
+  email = "From: #{SENDER_NAME} <#{SENDER_EMAIL}>\n"
+  email << "To: #{RECEIVER_NAME} <#{RECEIVER_EMAIL}>\n"
+  email << "Subject: #{EMAIL_SUBJECT}\n\n"
+  email << msg
+  Net::SMTP.start('localhost') do |smtp|
+  smtp.send_message(email, "#{SENDER_EMAIL}", "#{RECEIVER_EMAIL}")
+  end
+end
+
+def send_hipchat_notification(msg)
+  client = HipChat::Client.new(API_TOKEN, :api_version => 'v2')
+  client[ROOM_ID].send(USERNAME, msg)
+end
 
 headless = Headless.new
 headless.start
-
 browser = Watir::Browser.new
 browser.goto(LOGIN_URL)
 browser.text_field(:id, 'Email').set(LOGIN_EMAIL)
@@ -25,22 +53,19 @@ browser.text_field(:id, 'Passwd').set(LOGIN_PASSWORD)
 browser.button(:id, 'signIn').click
 browser.goto(QUARANTINE_URL)
 Watir::Wait.while { browser.text.match(/Loading \.\.\./) }
+sleep 3
 table = browser.tables[1]
 
 if table.rows.length > 1
-  msg = "From: #{SENDER_NAME} <#{SENDER_EMAIL}>\n"
-  msg << "To: #{RECEIVER_NAME} <#{RECEIVER_EMAIL}>\n"
-  msg << "Subject: #{EMAIL_SUBJECT}\n\n"
-  msg << "#{table.rows.length-1} emails are currently in quarantine.\n"
+  msg = "#{table.rows.length-1} emails are currently in quarantine.\n"
   table.rows.each do |r|
     msg << "---------------\n"
     msg << "#{r.text}\n"
   end
   msg << QUARANTINE_URL
-  puts msg
-  Net::SMTP.start('localhost') do |smtp|
-    smtp.send_message(msg, "#{SENDER_EMAIL}", "#{RECEIVER_EMAIL}")
-  end
+  puts msg if PRINT_RESULTS
+  send_email(msg) if SEND_EMAIL
+  send_hipchat_notification(msg) if SEND_HIPCHAT_NOTIFICATION
 end
 
 browser.close
